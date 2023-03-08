e21b4e87a2b4d9fec6e992418e6190a04c266e24
BohdanRybalka <021102rb@gmail.com>
2023-03-08 17:28:42 +0200

    onStart, onPause, onRestart, on Resume, onStop, onDestroy methods were added to ReceiveMessageActivity

--- LW-3/src/main/java/com/example/lw_3/ReceiveMessageActivity.java
+++ LW-3/src/main/java/com/example/lw_3/ReceiveMessageActivity.java
@@ -2,6 +2,7 @@ package com.example.lw_3;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +10,8 @@ import androidx.appcompat.app.AppCompatActivity;
 public class ReceiveMessageActivity extends AppCompatActivity {
 
     TextView textView;
+
+    final String TAG = "States";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,4 +26,41 @@ public class ReceiveMessageActivity extends AppCompatActivity {
         textView.setText(getMessage);
 
     }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(TAG, "ReceiveMessageActivity: onStart()");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d(TAG, "ReceiveMessageActivity: onRestart()");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(TAG, "ReceiveMessageActivity: onResume()");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(TAG, "ReceiveMessageActivity: onPause()");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(TAG, "ReceiveMessageActivity: onStop()");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "ReceiveMessageActivity: onDestroy()");
+    }
+
 }
